f59f4009fb79b0f7e8ab77e7b58cd0729c0d69c5
lingeswrn <lingeswrn@gmail.com>
2017-03-31 20:07:42 +0530

    Design and DB

---
+++ HTML/Design/assets/external/modal_item.php
@@ -0,0 +1,106 @@
+<div class="modal-item-detail modal-dialog" role="document" data-latitude="40.75408424" data-longitude="-73.87666225" data-address="">
+    <div class="modal-content">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <div class="section-title">
+                <h2>University Day</h2>
+                <div class="label label-default">Relax</div><div class="rating-passive" data-rating="5">
+                        <span class="stars"></span>
+                        <span class="reviews">10</span>
+                    </div><div class="controls-more">
+                    <ul>
+                        <li><a href="#">Add to favorites</a></li>
+                        <li><a href="#">Add to watchlist</a></li>
+                    </ul>
+                </div>
+                <!--end controls-more-->
+            </div>
+            <!--end section-title-->
+        </div>
+        <!--end modal-header-->
+        <div class="modal-body">
+            <div class="left"><div class="gallery owl-carousel" data-owl-nav="1" data-owl-dots="0"><img src="assets/img/items/12.jpg" alt=""><img src="assets/img/items/9.jpg" alt=""><img src="assets/img/items/11.jpg" alt=""></div>
+                    <!--end gallery--><div class="map" id="map-modal"></div>
+                <!--end map-->
+
+                <section>
+                <h3>Contact</h3><h5><i class="fa fa-map-marker"></i>Central Town University</h5></section>
+                <section>
+                    <h3>Social Share</h3>
+                    <div class="social-share"></div>
+                </section>
+            </div>
+            <!--end left -->
+            <div class="right">
+                <section>
+                    <h3>About</h3>
+                    <div class="read-more"><p>Duis nec lobortis massa. Mauris tempus vitae augue eu tempus</p></div>
+                </section>
+                <!--end about--><section>
+                        <h3>Schedule</h3><ul class="list-unstyled list-schedule">
+                                <li>
+                                    <div class="left">
+                                        <strong class="promoted">24.03.2017</strong>
+                                        <figure>16:00</figure>
+                                    </div>
+                                    <div class="right">
+                                        <strong>Town Square</strong>
+                                        <figure>458 Brigth Street London</figure>
+                                    </div>
+                                </li>
+                            </ul>
+                            <!--end list-schedule--><ul class="list-unstyled list-schedule">
+                                <li>
+                                    <div class="left">
+                                        <strong class="promoted">03.04.2017</strong>
+                                        <figure>18:00</figure>
+                                    </div>
+                                    <div class="right">
+                                        <strong>Bristol Gallery</strong>
+                                        <figure>87 Yellow Lane, Manhattan</figure>
+                                    </div>
+                                </li>
+                            </ul>
+                            <!--end list-schedule--></section>
+                    <!--end schedule--><section>
+                        <h3>Video presentation</h3>
+                        <div class="video"><iframe src='https://player.vimeo.com/video/184360631' width='640' height='360' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
+                    </section>
+                    <!--end video--><section>
+                        <h3>Latest reviews</h3><div class="review">
+                                <div class="image">
+                                    <div class="bg-transfer" style="background-image: url(assets/img/person-01.jpg)"></div>
+                                </div>
+                                <div class="description">
+                                    <figure>
+                                        <div class="rating-passive" data-rating="4">
+                                            <span class="stars"></span>
+                                        </div>
+                                        <span class="date">09.05.2016</span>
+                                    </figure>
+                                    <p>Morbi varius orci in rhoncus posuere. Sed cursus urna ut sem rhoncus lacinia. Praesentac velit dignissim, mollis purus quis, sollicitudin eros</p>
+                                </div>
+                            </div>
+                            <!--end review--><div class="review">
+                                <div class="image">
+                                    <div class="bg-transfer" style="background-image: url(assets/img/person-02.jpg)"></div>
+                                </div>
+                                <div class="description">
+                                    <figure>
+                                        <div class="rating-passive" data-rating="4">
+                                            <span class="stars"></span>
+                                        </div>
+                                        <span class="date">09.05.2016</span>
+                                    </figure>
+                                    <p>Donec nec tristique sapien. Aliquam ante felis, sagittis sodales diam sollicitudin, dapibus semper turpis</p>
+                                </div>
+                            </div>
+                            <!--end review--></section>
+                    <!--end reviews--></div>
+            <!--end right-->
+        </div>
+        <!--end modal-body-->
+    </div>
+    <!--end modal-content-->
+</div>
+<!--end modal-dialog-->
